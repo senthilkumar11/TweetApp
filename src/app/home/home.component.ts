6c722e4f903430c4acf21d205a7b0f39711b505f
AAP49ZZ <sselvaraj4.cw@mmm.com>
2021-05-14 23:33:42 +0530

    deletion bug fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,12 +15,12 @@ export class HomeComponent implements OnInit {
   tweet!: tweet;
   tweetList!: any[];
   newTweet!: string;
-  newReply!:string;
+  newReply!: string;
   isError: boolean = false;
   errorMsg: string = "Error";
-  page:number=0;
-  max:number=2;
-  hasNext:boolean=true;
+  page: number = 0;
+  max: number = 2;
+  hasNext: boolean = true;
   constructor(private authService: AuthService, private homeService: HomeServiceService) { }
 
   ngOnInit(): void {
@@ -28,9 +28,9 @@ export class HomeComponent implements OnInit {
     this.userId = this.authService.getUserId();
     this.getAllTweet();
   }
- 
+
   postTweet() {
-    this.isError=false;
+    this.isError = false;
     console.log(this.newTweet);
     if (this.newTweet === undefined || this.newTweet === null) {
       this.isError = true;
@@ -56,99 +56,100 @@ export class HomeComponent implements OnInit {
         return;
       }
     })
-    if(!this.isError){
-    let newTweet = new tweet();
-    newTweet.tweet = this.newTweet;
-    console.log(newTweet);
-    this.homeService.addTweet(newTweet).subscribe(data => {
-      console.log(data);
-      this.newTweet = "";
-      this.getAllTweet();
-    });
-  }
+    if (!this.isError) {
+      let newTweet = new tweet();
+      newTweet.tweet = this.newTweet;
+      console.log(newTweet);
+      this.homeService.addTweet(newTweet).subscribe(data => {
+        console.log(data);
+        this.newTweet = "";
+        this.getAllTweet();
+      });
+    }
   }
   getAllTweet() {
     this.homeService.getAllTweetsOfUser(this.authService.getUserId(), new Page(this.page, this.max)).subscribe((data: any) => {
-      if (data.length>0){
-        this.hasNext=true;
+      if (data.length > 0) {
+        this.hasNext = true;
         console.log(data);
-      this.tweetList = data.map((val: any) => {
-        var diffDays;
-        if (val.date) {
-          let tweetDate = new Date(val.date);
-          let sec= Math.floor((new Date().getTime() - tweetDate.getTime()) / 1000);
-          let min=Math.floor(sec/60);
-          let hours=Math.floor(min/60);
-          let days=Math.floor(hours/24);
-         
-         if(days>0)
-          diffDays = days + " days ago";
-          else if(hours>0)
-          diffDays = hours + " hours ago";
-          else if(min>0)
-          diffDays=min+" mins ago"
-          else 
-          diffDays=sec+" secs ago"
-        }
-        let like:string[]=[];
-        if (val.likes!=undefined&&val.likes!=null)
-          like = val.likes.map((data: any) => {
-            return data.id;
-          })
-       else{
-        like=[];
-       }
-       let reply:any[]=[];
-       if(val.reply!=undefined&&val.reply!=null){
-         reply=val.reply.map((data:any)=>{
+        this.tweetList = data.map((val: any) => {
           var diffDays;
-          if (data.date) {
-            let tweetDate = new Date(data.date);
-            let sec= Math.floor((new Date().getTime() - tweetDate.getTime()) / 1000);
-            let min=Math.floor(sec/60);
-            let hours=Math.floor(min/60);
-            let days=Math.floor(hours/24);
-           
-           if(days>0)
-            diffDays = days + " days ago";
-            else if(hours>0)
-            diffDays = hours + " hours ago";
-            else if(min>0)
-            diffDays=min+" mins ago"
-            else 
-            diffDays=sec+" secs ago"
+          if (val.date) {
+            let tweetDate = new Date(val.date);
+            let sec = Math.floor((new Date().getTime() - tweetDate.getTime()) / 1000);
+            let min = Math.floor(sec / 60);
+            let hours = Math.floor(min / 60);
+            let days = Math.floor(hours / 24);
+
+            if (days > 0)
+              diffDays = days + " days ago";
+            else if (hours > 0)
+              diffDays = hours + " hours ago";
+            else if (min > 0)
+              diffDays = min + " mins ago"
+            else
+              diffDays = sec + " secs ago"
           }
-           return{
-            id: data.id,
-            tweet: data.tweet,
+          let like: string[] = [];
+          if (val.likes != undefined && val.likes != null)
+            like = val.likes.map((data: any) => {
+              return data.id;
+            })
+          else {
+            like = [];
+          }
+          let reply: any[] = [];
+          if (val.reply != undefined && val.reply != null) {
+            reply = val.reply.map((data: any) => {
+              var diffDays;
+              if (data.date) {
+                let tweetDate = new Date(data.date);
+                let sec = Math.floor((new Date().getTime() - tweetDate.getTime()) / 1000);
+                let min = Math.floor(sec / 60);
+                let hours = Math.floor(min / 60);
+                let days = Math.floor(hours / 24);
+
+                if (days > 0)
+                  diffDays = days + " days ago";
+                else if (hours > 0)
+                  diffDays = hours + " hours ago";
+                else if (min > 0)
+                  diffDays = min + " mins ago"
+                else
+                  diffDays = sec + " secs ago"
+              }
+              return {
+                id: data.id,
+                tweet: data.tweet,
+                date: diffDays,
+                user: data.user,
+              }
+            })
+          }
+          return {
+            id: val.id,
+            tweet: val.tweet,
             date: diffDays,
-            user: data.user,
-           }
-         })
-       }
-        return {
-          id: val.id,
-          tweet: val.tweet,
-          date: diffDays,
-          user: val.user,
-          reply: reply,
-          like: like,
-          replyInputEnable:false
-        }
-      });
-    }else{
-      this.hasNext=false;
-    }
+            user: val.user,
+            reply: reply,
+            like: like,
+            replyInputEnable: false
+          }
+        });
+      } else {
+        this.hasNext = false;
+        this.tweetList=[];
+      }
 
       console.log(this.tweetList);
     })
 
   }
-  next(){
+  next() {
     this.page++;
     this.getAllTweet();
   }
-  prev(){
+  prev() {
     this.page--
     this.getAllTweet();
   }
